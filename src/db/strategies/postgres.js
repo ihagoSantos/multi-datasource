b4ae7edad4f57c1502b68721099980a527154e49
Ihago Freire Santos <ihago.santos@compasso.com.br>
2021-11-18 14:18:24 -0300

    crud with postgres

--- src/db/strategies/postgres.js
+++ src/db/strategies/postgres.js
@@ -1,12 +1,82 @@
 const ICrud = require('./interfaces/interfaceCrud')
+const Sequelize = require('sequelize')
 
 class Postgres extends ICrud {
     constructor(){
         super()
+        this._driver = null
+        this._herois = null
     }
 
-    create(item) {
-        console.log('O item foi salvo em Postgres')
+    async isConnected(){
+        try {
+            await this._driver.authenticate()
+            return true
+        } catch (error) {
+            console.log('fail!', error)
+            return false
+        }
+    }
+
+    async connect() {
+        this._driver = new Sequelize(
+            'heroes',
+            'root',
+            'root',
+            {
+                host: 'localhost',
+                dialect: 'postgres',
+                quoteIdentifiers: false,
+                operatorsAliases: false
+            }
+        )    
+        await this.defineModel()    
+    }
+
+    async defineModel() {
+        this._herois = this._driver.define('herois', {
+            id: {
+                type: Sequelize.INTEGER,
+                required: true,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            nome: {
+                type: Sequelize.STRING,
+                required: true,
+            },
+            poder: {
+                type: Sequelize.STRING,
+                required: true,
+            },
+        }, {
+            tableName: 'TB_HEROIS',
+            freezeTableName: false,
+            timestamps: false
+        })
+    
+        await this._herois.sync()
+    }
+
+    async create(item) {
+        const {dataValues} = await this._herois.create(item)
+        return dataValues
+    }
+
+    async read(query = {}) {
+        return this._herois.findAll({ where: query, raw: true })
+
+    }
+    async update(id, item) {
+        const result = await this._herois.update(item, {
+            where: { id: id}
+        })
+
+        return result
+    }
+    async delete(id) {
+        const query = id ? { id } : {}
+        return this._herois.destroy({ where: query })
     }
 }
 
