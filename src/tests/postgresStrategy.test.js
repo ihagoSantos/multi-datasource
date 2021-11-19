c8efcd3f83045cf5ce06f8532895412f4c886372
Ihago Freire Santos <ihago.santos@compasso.com.br>
2021-11-19 14:52:49 -0300

    postgres refactoring

--- src/tests/postgresStrategy.test.js
+++ src/tests/postgresStrategy.test.js
@@ -1,8 +1,9 @@
 const assert = require('assert')
-const Postgres = require('../db/strategies/postgres')
+const Postgres = require('../db/strategies/postgres/postgres')
+const HeroiSchema = require('./../db/strategies/postgres/schemas/HeroiSchema')
 const Context = require('../db/strategies/base/contextStrategy')
 
-const context = new Context(new Postgres())
+// const context = new Context(new Postgres())
 const MOCK_HEROI_CADASTRAR = {
     nome: 'Gavião Negro',
     poder: 'Flexas'
@@ -11,12 +12,15 @@ const MOCK_HEROI_ATUALIZAR = {
     nome: 'Batman',
     poder: 'Dinheiro'
 }
+let context = {}
 
 describe('Postgres Strategy', function () {
     this.timeout(Infinity)
     
     this.beforeAll(async function(){
-        await context.connect()
+        const connection = await Postgres.connect()
+        const model = await Postgres.defineModel(connection, HeroiSchema)
+        context = new Context(new Postgres(connection, model))
         await context.delete()
         await context.create(MOCK_HEROI_ATUALIZAR)
     })
